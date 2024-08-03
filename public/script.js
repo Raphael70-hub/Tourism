80e7efe6ad1d12a077b66184fe6f64fc5a6b7641
Raphael70-hub <raphaelolagoke70@gmail.com>
2024-08-03 21:53:24 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -15,5 +15,45 @@ $(document).ready(function() {
      
    // document ready  
    });
+
+
+   window.addEventListener("resize", function(){
+    if(window.innerWidth <= 900 ){
+        var destopCarousel = document.querySelector("#carousel-item1");
+        destopCarousel.style.height = "200px"
+
+        var destopCarousel = document.querySelector("#carousel-item2");
+        destopCarousel.style.height = "200px"
+
+        var destopCarousel = document.querySelector("#carousel-item3");
+        destopCarousel.style.height = "200px"
+
+        var image = document.querySelector("#Image");
+        image.width = 240;
+        image.height = 160;
+
+    //     var mobilecarousel = document.querySelector("#mobilecarousel");
+    // mobilecarousel.style.display = "block"
+
+    }
+    else{
+      var destopCarousel = document.querySelector("#carousel-item1");
+      destopCarousel.style.height = "650px"
+
+      var destopCarousel = document.querySelector("#carousel-item2");
+      destopCarousel.style.height = "650px"
+
+      var destopCarousel = document.querySelector("#carousel-item3");
+      destopCarousel.style.height = "650px"
+
+        var image = document.querySelector("#Image");
+        image.width = 500;
+        image.height = 500;
+
+    //     var mobilecarousel = document.querySelector("#mobilecarousel");
+    // mobilecarousel.style.display = "none"
+    }
+
+})
    
    
